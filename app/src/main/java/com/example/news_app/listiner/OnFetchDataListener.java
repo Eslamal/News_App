273f2d288644bea81e180a6deb22394aacb763b2
Eslamal <eslameng776@gmail.com>
2023-12-07 17:27:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/news_app/listiner/OnFetchDataListener.java
@@ -0,0 +1,12 @@
+package com.example.news_app.listiner;
+
+
+
+import com.example.news_app.model.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse>{
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/news_app/listiner/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.news_app.listiner;
+
+import com.example.news_app.model.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
